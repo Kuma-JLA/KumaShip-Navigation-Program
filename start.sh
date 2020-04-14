b38f6749e063923cbfcf5a92ba26a6f55c8a3e27
球磨 <59664749+Kuma-JLA@users.noreply.github.com>
2020-04-14 23:53:19 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#安定まで10秒待機します
-sleep 10
+#安定まで20秒待機します
+sleep 20
 sudo ifup rokemoba
 sudo pigpiod
-#GPS測位までさらに20秒待機します
-sleep 20
+#GPS測位までさらに10秒待機します
+sleep 10
 sudo python3 (clone先フォルダ)/KumaShip-Navigation-Program/base.py
